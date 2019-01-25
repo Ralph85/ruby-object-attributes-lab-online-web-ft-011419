e251d2d9a002dc695bfb90d4eda5663715cc05f3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-25 03:27:07 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,19 @@
 class Person
-  #name
-  #name=
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 
 end
